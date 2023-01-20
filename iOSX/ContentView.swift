2956f97977aa1fdc2c6a68dc826ba31357d804eb
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-01-20 16:46:34 -0500

    Initial Commit

---
+++ iOSX/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOSX
+//
+//  Created by Xavier on 1/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSX/iOSXApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSXApp.swift
+//  iOSX
+//
+//  Created by Xavier on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
